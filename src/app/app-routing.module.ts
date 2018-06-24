cbfecc9248d31c7c3ac36dc657e0f818f178f6a0
A. Torres Sampaio <a.torres.sampaio@avanade.com>
2018-06-24 19:24:02 -0300

    Fix roting model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AuthenticationGuard } from './@services/authentication.guard';
 
 const routes: Routes = [
   { path: 'info', loadChildren: './@modules/utility/utility.module#UtilityModule' },
-  { path: 'user', loadChildren: './@modules/user/user.module#UserModule', canActivate[AuthenticationGuard] },
+  { path: 'user', loadChildren: './@modules/user/user.module#UserModule', canActivate: [AuthenticationGuard] },
   { path: '', loadChildren: './@modules/landing/landing.module#LandingModule' },
   { path: '**', redirectTo: '/info/notfound', pathMatch: 'full' }
 ];
